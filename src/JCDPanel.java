308ceb06360b2169bba7ad4deab254e1370fa5d0
andreig110 <42849998+andreig110@users.noreply.github.com>
2019-01-18 22:27:01 +0200

    Create src/JCDPanel.java

---
+++ src/JCDPanel.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class JCDPanel extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
